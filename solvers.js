803a0e4acef923b7bb1e4ce9988efcd65a4b542a
phillipalexander <git@phillipalexander.io>
2013-06-27 16:32:09 -0700

    fix countNRooksSolutions()

--- solvers.js
+++ solvers.js
@@ -2,41 +2,50 @@
 // hint: you'll need to do a full-search of all possible arrangements of pieces!
 // (There are also optimizations that will allow you to skip a lot of the dead search space)
 
-window.findNRooksSolution = function(n){
+window.findNRooksSolution = function (n) {
   var solution = undefined; //fixme
 
   console.log('Single solution for ' + n + ' rooks:', solution);
   return solution;
 };
 
-window.countNRooksSolutions = function(n){
-  var solutionCount = undefined; //fixme
+window.countNRooksSolutions = function (n) {
+
+  var solutionCount = function (n) {
+    if (n < 0) {
+      return;
+    }
+    // Base case
+    if (n === 0) {
+      return 1;
+    }
+    // Recursive case
+    return n * solutionCount(n - 1);
+  };
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  return solutionCount(n);
 };
 
-window.findNQueensSolution = function(n){
+window.findNQueensSolution = function (n) {
   var solution = undefined; //fixme
 
   console.log('Single solution for ' + n + ' queens:', solution);
   return solution;
 };
 
-window.countNQueensSolutions = function(n){
+window.countNQueensSolutions = function (n) {
   var solutionCount = undefined; //fixme
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
 
-
 // This function uses a board visualizer lets you view an interactive version of any piece matrix.
 
-window.displayBoard = function(matrix){
+window.displayBoard = function (matrix) {
   $('body').html(
     new BoardView({
-      model: new Board(matrix)
-    }).render()
-  );
+    model: new Board(matrix)
+  }).render());
 };
